6c136bc381630546d3cfae2f0319da1633b17f18
David Lidebrandt <david.lidebrandt@gmail.com>
2023-09-22 20:49:05 +0200

    Add class and functions for general discounts

--- src/main/java/com/products/App.java
+++ src/main/java/com/products/App.java
@@ -1,5 +1,8 @@
 package com.products;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) {
         Product milk = new Product("MILK", 100.0, 6);
@@ -13,5 +16,13 @@ public class App {
         System.out.println(discount.getDescription(tv));
         System.out.println(discount.apply(shoe));
         System.out.println(discount.getDescription(shoe));
+
+        GeneralDiscount milkDiscount = new GeneralDiscount((Product p) -> { return p.name() == "MILK";}, (Product p) -> {return p.price() * p.quantity() * 0.05;});
+        GeneralDiscount quantityDiscount = new GeneralDiscount(milkDiscount, (Product p) -> {return p.quantity() > 4;}, (Product p) -> {return p.quantity() * 10.0;});
+        GeneralDiscount allDiscounts = new GeneralDiscount(quantityDiscount, (Product p) -> {return LocalDate.now().getDayOfWeek() == DayOfWeek.FRIDAY;}, (Product p) -> {return p.price() * p.quantity() * 0.1;});
+
+        System.out.println(allDiscounts.apply(milk));
+        System.out.println(allDiscounts.apply(tv));
+        System.out.println(allDiscounts.apply(shoe));
     }
 }
---
+++ src/main/java/com/products/GeneralDiscount.java
@@ -0,0 +1,34 @@
+package com.products;
+
+import java.util.function.Function;
+
+public class GeneralDiscount {
+    GeneralDiscount nextGeneralDiscount;
+    Function<Product, Boolean> isApplicable;
+    Function<Product, Double> calculateDiscount;
+
+    public GeneralDiscount(Function<Product, Boolean> isApplicable, Function<Product, Double> calculateDiscount) {
+        this.isApplicable = isApplicable;
+        this.calculateDiscount = calculateDiscount;
+    }
+
+    public GeneralDiscount(GeneralDiscount nextGeneralDiscount,
+            Function<Product, Boolean> isApplicable, Function<Product, Double> calculateDiscount) {
+        this.nextGeneralDiscount = nextGeneralDiscount;
+        this.isApplicable = isApplicable;
+        this.calculateDiscount = calculateDiscount;
+    }
+
+    public double apply(Product product) {
+        double nextDiscountSum = 0;
+        if (nextGeneralDiscount != null) {
+            nextDiscountSum = nextGeneralDiscount.apply(product);
+        }
+
+        if (isApplicable.apply(product)) {
+            return nextDiscountSum + calculateDiscount.apply(product);
+        }
+        return 0.0;
+    }
+
+}
